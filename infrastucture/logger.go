3ec39dcb57033113de92f80324346fe48f092ee1
lkumbrella <L.Kumbrella@outlook.com>
2023-12-26 01:04:07 +0300

    configuration & IoC & project structure

---
+++ infrastucture/logger.go
@@ -0,0 +1,47 @@
+package infrastucture
+
+import (
+	"context"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"os"
+	"strings"
+)
+
+func NewLogger(ctx context.Context, config *Config) (*zap.Logger, error) {
+	stdout := zapcore.AddSync(os.Stdout)
+	stderr := zapcore.AddSync(os.Stderr)
+	level := getLogLevel(config)
+	loggerCfg := zap.NewDevelopmentEncoderConfig()
+	if os.Getenv("APP_ENV") == "production" {
+		loggerCfg = zap.NewProductionEncoderConfig()
+	}
+
+	consoleEncoder := zapcore.NewConsoleEncoder(loggerCfg)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(consoleEncoder, stdout, level),
+		zapcore.NewCore(consoleEncoder, stderr, level),
+	)
+
+	logger := zap.New(core)
+
+	defer func(logger *zap.Logger) {
+		// Error is written if OS didn't take care of flushing buffers out
+		if err := logger.Sync(); err != nil && !strings.Contains(err.Error(), "sync /dev/stderr: The handle is invalid.") {
+			log.Fatalf("can't sync zap logger: %v", err)
+		}
+	}(logger)
+
+	return logger, nil
+}
+
+func getLogLevel(config *Config) zap.AtomicLevel {
+	level := zap.NewAtomicLevel()
+	err := level.UnmarshalText([]byte(config.LogLevel))
+	if err != nil {
+		return zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
+	return level
+}
